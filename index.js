aa94182158e34f0bdb54980a37dee6e92c707c79
Geovanny Virgen Luna <geoluna8@gmail.com>
2025-04-25 00:55:24 -0600

    Calendar App Deployment

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const path = require( 'path' );
+
 const express = require('express');
 const { dbConnection } = require('./database/config');
 const cors = require('cors');
@@ -23,6 +25,10 @@ app.use( express.json() );
 // Rutas
 app.use('/api/auth', require('./routes/auth') );
 app.use('/api/events', require('./routes/events') );
+
+app.use( '/{*any}', (req, res) => {
+    res.sendFile( path.join( __dirname, 'public/index.html') );
+} );
 
 // TODO: CRUD: Eventos
 
